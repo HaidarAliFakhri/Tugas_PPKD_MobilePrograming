f26167c619f02d84d184480f9e35a1cdf883f1a2
haidar ali fakhri <haidar.ali.fakhri.999@gmail.com>
2025-10-08 10:33:48 +0700

    Update tugas3.dart

--- tugas3.dart
+++ tugas3.dart
@@ -1,26 +1,30 @@
 import 'dart:io';
-
+//soal 1
 void main() {
   for (int i = 1; i <= 20; i++) {
     if (i % 2 != 0) {
       stdout.write('$i ');
     }
   }
-print('\n');
+  print('\n');//memberi space output
+  //soal 2
   for (int i = 0; i < 5; i++) {
     stdout.write('* ');
   }
-print('\n');
+  print('\n');
+  //soal 3
   List<String> nama = ['haidar', 'haidar', 'haidar'];
   for (var n in nama) {
     print("$n");
   }
-print('\n');
+  print('\n');
+  //soal 4
   List<String> buah = ['Apel', 'Jeruk', 'Mangga', 'Anggur'];
   for (var n in buah) {
     print("Saya Suka $n");
   }
-print('\n');
+  print('\n');
+  //soal 5
   List<String> daftarBelanja = [
     'Beras',
     'Minyak',
@@ -30,8 +34,7 @@ print('\n');
     'Micin',
   ];
 
-  for (int i = 0; i < daftarBelanja.length; i++) {
+  for (int i = 0; i < daftarBelanja.length; i++) {//length untuk mengetahui jumlah elemen
     print('Item ke-${i + 1}: ${daftarBelanja[i]}');
   }
 }
-
